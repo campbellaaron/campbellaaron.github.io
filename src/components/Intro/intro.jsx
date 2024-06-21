6d1caa1b7095b2a17cb3644b36b1e875340e104f
Aaron Campbell <aaronc@petaf.org>
2024-06-21 18:51:14 -0400

    Fixed HireMe button

--- src/components/Intro/intro.jsx
+++ src/components/Intro/intro.jsx
@@ -9,7 +9,7 @@ const Intro = () => {
                 <p className="info__greeting">Hello,</p>
                 <h1 className="info__intro">I'm <span className="name">Aaron Campbell</span>, <br/>Web & Game Developer</h1>
                 <p className="info__description">I am a skilled WordPress developer with additional training in game development</p>
-                <Link><button className="hireBtn" onClick={() => {document.getElementById('contact').scrollIntoView({behavior: 'smooth'});}}><img src={IMAGES.iconBriefcase} alt="Briefcase icon" /> Hire Me</button></Link>
+                <button className="hireBtn" onClick={() => {document.getElementById('contact').scrollIntoView({behavior: 'smooth'});}}><img src={IMAGES.iconBriefcase} alt="Briefcase icon" /> Hire Me</button>
             </div>
             <div className="img col">
                 <img src={IMAGES.imageAWC} alt="Aaron Profile Pic" />
